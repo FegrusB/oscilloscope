c1d8c22ed254f6b78f6b45d0c5bd688796a77652
FegrusB <fergus@beckerleg.co.uk>
2026-06-19 17:36:40 +0100

    basic square wave working

--- src/main/java/com/example/oscilloscope/HelloController.java
+++ src/main/java/com/example/oscilloscope/HelloController.java
@@ -1,27 +1,62 @@
 package com.example.oscilloscope;
 
 import com.example.oscilloscope.signalService.SignalService;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
 public class HelloController {
 
     private SignalService signalService;
     private double signalValue;
+    private static final int MAX_POINTS = 200;
+    private long startTime = 0;
 
-    public HelloController() {
+    @FXML private LineChart<Number,Number> scopeView;
+    @FXML private final XYChart.Series<Number,Number> signalSeries = new XYChart.Series<>();
+    @FXML private NumberAxis xAxis;
+    @FXML private NumberAxis yAxis;
+
+    @FXML
+    private void initialize() {
         signalService = new SignalService();
         signalService.startSignal(1,0.5);
+
+        xAxis.setAutoRanging(true);
+        yAxis.setAutoRanging(true);
+
+        xAxis.setAnimated(false);
+        xAxis.setAnimated(false);
+
+        scopeView.setAnimated(false);
+        scopeView.setCreateSymbols(false);
+
+        signalSeries.setName("signal 1");
+        scopeView.getData().add(signalSeries);
+
+        startTime = System.currentTimeMillis();
+
+
+    }
+
+    private void startScope(){
+        new AnimationTimer(){
+            @Override
+            public void handle(long now) {
+                double x = (double) (System.currentTimeMillis() - startTime) /1000;
+                double y = signalService.getSignal(0).signalValue.get();
+
+                signalSeries.getData().add(new XYChart.Data<>(x,y));
+            }
+        }.start();
     }
 
     @FXML
-    private Label welcomeText = new Label();
-
-
-
-    @FXML
     protected void onHelloButtonClick() {
-        signalValue = signalService.getSignal(0).signalValue.get();
-        welcomeText.setText(String.valueOf(signalValue));
+        startScope();
     }
 }
