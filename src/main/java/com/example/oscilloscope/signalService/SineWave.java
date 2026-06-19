24b8443b2b4e8d66393737ea4c2ddb4bbba5a6c3
FegrusB <fergus@beckerleg.co.uk>
2026-06-19 18:10:15 +0100

    sine wave formula

---
+++ src/main/java/com/example/oscilloscope/signalService/SineWave.java
@@ -0,0 +1,41 @@
+package com.example.oscilloscope.signalService;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SineWave implements ISignal {
+
+    private final AtomicReference<Double> signalValue = new AtomicReference<>(0.0);
+
+    private final long startTime = System.currentTimeMillis();
+
+    private final double angularFrequency;
+
+    Thread signalThread;
+
+    public SineWave(double amplitude, double frequency, double updates) {
+
+        this.angularFrequency = 2 * (Math.PI * frequency);
+        long wait = (long) (1000 / updates);
+
+        signalThread = new Thread(() -> {
+            signalValue.set(0.0);
+            while (true) {
+
+                signalValue.set( Math.sin(angularFrequency * (System.currentTimeMillis() - startTime)) * amplitude );
+
+                try {
+                    Thread.sleep(wait);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        signalThread.setDaemon(true);
+        signalThread.start();
+    }
+
+    public Double getSignal() {
+        return signalValue.get();
+    }
+
+}
